fb7add8d2cb2f29d55f5cf6c407e302239db53ff
RamintaRam <raminta5@yahoo.com>
2017-06-01 15:21:46 +0300

    css
    black menu

--- resources/views/frontend/includes/css.blade.php
+++ resources/views/frontend/includes/css.blade.php
@@ -1,2 +1,23 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="{{asset('css/frontend.css')}}">
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/frontend.css')}}">
+
+    <style>
+        #menu
+        {
+            background-color: black;
+        }
+
+
+
+    </style>
+</head>
+<body>
+
+
+</body>
+</html>
